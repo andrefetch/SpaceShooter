8a6d5e6726c4365a9a8c226f7cd0b799445c6a17
an3243 <an3243@drexel.edu>
2026-05-27 18:57:49 -0400

    Drew stars onto the screen.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
+from os.path import join
+from random import randint
 
 # General Setup
 pygame.init() 
 WIDTH = 1280
 HEIGHT = 720
 
-display_surace = pygame.display.set_mode((WIDTH, HEIGHT))
+display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
 
@@ -15,7 +17,9 @@ surf.fill('orange')
 x = 100
 
 # Importing Images
-player_surf = pygame.image.load('sprites/ship.png')
+player_surf = pygame.image.load(join('sprites', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('sprites', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WIDTH), randint(0, HEIGHT)) for i in range(20)]
 
 # Drawing Screen
 while running:
@@ -26,10 +30,13 @@ while running:
             running = False
 
 
-    # draw the game
-    display_surace.fill('darkgray')
+    # draw the game, drawing matters in lines, display background first, then stars, then ship
+    display_surface.fill('darkgray')
+
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
     x += 1
-    display_surace.blit(surf, ( x, 150 ))
+    display_surface.blit(player_surf, ( x, 150 ))
     pygame.display.update()
 
 pygame.quit()
