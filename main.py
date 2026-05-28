51ab5b7989cd1c96eb27d10c6e3c7d9121feafae
an3243 <an3243@drexel.edu>
2026-05-27 23:40:49 -0400

    Added: background

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ x = 100
 # Player
 player_surf = pygame.image.load(join('sprites', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WIDTH / 2, HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 0)
-player_speed = 100
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 200
 
 # Stars
 star_surf = pygame.image.load(join('sprites', 'star.png')).convert_alpha()
-star_positions = [(randint(0, WIDTH), randint(0, HEIGHT)) for i in range(20)]
+star_positions = [[randint(0, WIDTH), randint(0, HEIGHT)] for i in range(20)]
+star_speed = 300
 
 # Asteroid
 asteroid_surf = pygame.image.load(join('sprites', 'asteroid.png')).convert_alpha()
@@ -34,6 +35,11 @@ asteroid_rect = asteroid_surf.get_frect(center = (WIDTH / 2, HEIGHT / 2))
 # Laser
 laser_surf = pygame.image.load(join('sprites', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, HEIGHT - 20))
+
+# Background
+background_surf = pygame.image.load(join('sprites', 'background.png'))
+background_surf = pygame.transform.scale(background_surf, (WIDTH, HEIGHT))
+
 
 # Drawing Screen
 while running:
@@ -47,15 +53,24 @@ while running:
 
 
     # draw the game, drawing matters in lines, display background first, then stars, then ship
-    display_surface.fill('darkgray')
+    # display_surface.fill('darkgray')
+    display_surface.blit(background_surf, (0, 0))
 
     for pos in star_positions:
+        pos[1] += star_speed * dt
+        if pos[1] > HEIGHT:
+            pos[1] = 0
+            pos[0] = randint(0, WIDTH)
         display_surface.blit(star_surf, pos)
 
     display_surface.blit(asteroid_surf, asteroid_rect)
     display_surface.blit(laser_surf, laser_rect)
 
     # Player Movement
+    if player_rect.bottom >= HEIGHT or player_rect.top <= 0:
+        player_direction.y *= -1
+    if player_rect.right >= WIDTH or player_rect.left <= 0:
+        player_direction.x *= -1
     player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf, player_rect)
 
